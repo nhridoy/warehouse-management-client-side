37dba875fdf4b6849a875b77a3dce37904d06b1b
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2022-05-01 18:21:40 +0600

    Single Item Show

--- src/pages/Inventory/Inventory.js
+++ src/pages/Inventory/Inventory.js
@@ -1,8 +1,11 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
 import { useForm } from "react-hook-form";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 const Inventory = () => {
+  const { id } = useParams();
+  const [item, setItem] = React.useState({});
   const {
     register,
     handleSubmit,
@@ -12,25 +15,33 @@ const Inventory = () => {
   const onSubmit = (data) => {
     console.log(data);
   };
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/items/${id}`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      })
+      .then((res) => {
+        setItem(res.data);
+      });
+  }, []);
   return (
     <div className="container mx-auto mt-20">
-      <div className="grid grid-cols-2 gap-4">
+      <div className="grid grid-cols-2 items-center gap-4">
         <div className="p-4 border rounded-lg">
-          <img
-            className=""
-            src="https://demo.themesuite.com/automotive-wp/wp-content/uploads/2014/09/car1-lrg1.jpg"
-          />
+          <img className=" h-96 w-full object-cover" src={item.image} />
         </div>
         <div className="">
-          <p className="font-semibold text-gray-900">ID: 654546</p>
-          <p className="font-semibold text-gray-900">Name: sdfdf</p>
-          <p className="font-semibold text-gray-900">Supplier Name: sdfdf</p>
-          <p className="text-gray-900">
-            Short Descripttion Short Descripttion Short Descripttion Short
-            Descripttion Short Descripttion
+          <p className="font-semibold text-gray-900">ID: {item._id}</p>
+          <p className="font-semibold text-gray-900">Name: {item.name}</p>
+          <p className="font-semibold text-gray-900">
+            Supplier Name: {item.supplier}
           </p>
-          <p className="text-gray-900">500.00</p>
-          <p className="text-gray-900">50 pc</p>
+          <p className="text-gray-900">Description: {item.description}</p>
+          <p className="text-gray-900">{item.price}</p>
+          <p className="text-gray-900">{item.quantity} pc</p>
 
           <button className="mt-4 text-white bg-gray-900 border-2 border-gray-900 rounded-md px-2 py-1">
             Delivered
