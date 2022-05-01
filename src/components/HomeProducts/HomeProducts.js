3a29d805166bb9afd4ffc5838451a9da7fabc23f
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2022-05-01 17:55:16 +0600

    Top Items

--- src/components/HomeProducts/HomeProducts.js
+++ src/components/HomeProducts/HomeProducts.js
@@ -1,11 +1,20 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const HomeProducts = () => {
+  const [data, setData] = React.useState([]);
   const navigate = useNavigate();
   const onUpdateClick = (id) => {
     navigate(`/inventory/${id}`);
   };
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/topitems").then((res) => {
+      setData(res.data);
+    });
+  }, []);
+
   return (
     <div className="container p-4 mx-auto">
       <div className="flex justify-between items-center">
@@ -14,29 +23,27 @@ const HomeProducts = () => {
         </h2>
       </div>
       <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4 mt-4">
-        <div className="p-4 border rounded-lg">
-          <img
-            className=""
-            src="https://demo.themesuite.com/automotive-wp/wp-content/uploads/2014/09/car1-lrg1.jpg"
-          />
-          <div className="mt-2">
-            <div className="">
-              <p className="font-semibold text-gray-900">Name</p>
-              <p className="text-gray-900">
-                Short Descripttion Short Descripttion Short Descripttion Short
-                Descripttion Short Descripttion
-              </p>
-              <p className="text-gray-900">500.00</p>
-              <p className="text-gray-900">50 pc</p>
+        {data?.map((item) => (
+          <div className="p-4 border rounded-lg shadow-md">
+            <img className="w-full h-80 object-cover" src={item?.image} />
+            <div className="mt-2">
+              <div className="">
+                <p className="font-semibold text-gray-900">{item?.name}</p>
+                <p className="text-gray-900">
+                  {item?.description.substring(0, 50)}...
+                </p>
+                <p className="text-gray-900">{item?.price}</p>
+                <p className="text-gray-900">{item?.quantity} pc</p>
+              </div>
+              <button
+                onClick={() => onUpdateClick(item?._id)}
+                className="mt-4 text-white bg-gray-900 border-2 border-gray-900 rounded-md px-2 py-1"
+              >
+                Update
+              </button>
             </div>
-            <button
-              onClick={() => onUpdateClick(5)}
-              className="mt-4 text-white bg-gray-900 border-2 border-gray-900 rounded-md px-2 py-1"
-            >
-              Update
-            </button>
           </div>
-        </div>
+        ))}
       </div>
       <div className="text-center">
         <button
