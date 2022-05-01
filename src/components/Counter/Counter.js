712a7a50a7aa406957348c34d3a5e07da56fdc60
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2022-05-01 23:43:58 +0600

    Home Counter

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,13 +1,34 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
 import { CountUp } from "use-count-up";
 
 const Counter = () => {
+  const [data, setData] = React.useState({});
+  useEffect(() => {
+    axios.get("http://127.0.0.1:5000/itemsummary").then((res) => {
+      setData(res.data);
+    });
+  }, []);
   return (
     <div className="bg-[url('https://demo.themesuite.com/automotive-wp/wp-content/uploads/2014/09/car1-lrg1.jpg')] bg-no-repeat bg-cover bg-center sepia p-10">
       <div className="bg-black opacity-80 p-10 text-white font-light text-3xl flex justify-around">
         <div className="flex flex-col items-center justify-center">
-          <CountUp thousandsSeparator="," isCounting end={23320} duration={5} />
-          <p>Total Product</p>
+          <CountUp
+            thousandsSeparator=","
+            isCounting
+            end={data.totalItems}
+            duration={5}
+          />
+          <p>Total Models</p>
+        </div>
+        <div className="flex flex-col items-center justify-center">
+          <CountUp
+            thousandsSeparator=","
+            isCounting
+            end={data.totalQuantity}
+            duration={5}
+          />
+          <p>Total Quantity</p>
         </div>
         <div className="flex flex-col items-center justify-center">
           <span>
@@ -15,7 +36,7 @@ const Counter = () => {
             <CountUp
               thousandsSeparator=","
               isCounting
-              end={13420}
+              end={data.totalValue}
               duration={5}
             />
           </span>
