6ae41e172075afb2521d2e986937dae893777cc9
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2022-05-02 11:36:12 +0600

    Counter Header

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -11,8 +11,8 @@ const Counter = () => {
   }, []);
   return (
     <div className="bg-[url('https://demo.themesuite.com/automotive-wp/wp-content/uploads/2014/09/car1-lrg1.jpg')] bg-no-repeat bg-cover bg-center sepia p-10">
-      <div className="bg-black opacity-80 p-10 text-white font-light text-3xl flex justify-around">
-        <div className="flex flex-col items-center justify-center">
+      <div className="bg-black opacity-80 p-10 text-white font-light md:text-3xl flex justify-around">
+        <div className="flex flex-col items-center text-center justify-center">
           <CountUp
             thousandsSeparator=","
             isCounting
@@ -21,7 +21,7 @@ const Counter = () => {
           />
           <p>Total Models</p>
         </div>
-        <div className="flex flex-col items-center justify-center">
+        <div className="flex flex-col items-center text-center justify-center">
           <CountUp
             thousandsSeparator=","
             isCounting
@@ -30,7 +30,7 @@ const Counter = () => {
           />
           <p>Total Quantity</p>
         </div>
-        <div className="flex flex-col items-center justify-center">
+        <div className="flex flex-col items-center text-center justify-center">
           <span>
             $
             <CountUp
